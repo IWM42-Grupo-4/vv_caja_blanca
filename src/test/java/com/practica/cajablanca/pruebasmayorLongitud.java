2368c7bc27c2ccbba34799075b8ea21fe06a44f3
alvaro.mgarcimartin <alvaro.mgarcimartin@alumnos.upm.es>
2021-11-24 20:22:25 +0100

    Modificación numPalabras y mayorLongitud

--- src/test/java/com/practica/cajablanca/pruebasmayorLongitud.java
+++ src/test/java/com/practica/cajablanca/pruebasmayorLongitud.java
@@ -1,7 +1,6 @@
 package com.practica.cajablanca;
 
 import com.cajanegra.EmptyCollectionException;
-import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,50 +10,39 @@ public class pruebasmayorLongitud {
     @DisplayName("Test para camino 1-2-3-16")
     @Test
     public void PruebamayorLongitud() throws EmptyCollectionException{
-        //SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>();
-        //SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>();
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>();
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
         assertEquals(null, editor.mayorLongitud());
     }
 
     @DisplayName("Test para camino 1-2-3-4-5-6-4-16")
     @Test
     public void PruebamayorLongitud1() throws EmptyCollectionException{
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>();
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestmayorLongitud/testEditorVacio.txt");
         assertEquals(null, editor.mayorLongitud());
     }
 
     @DisplayName("Test para camino 1-2-3-4-5-6-7-8-9-6-4-16")
     @Test
     public void PruebamayorLongitud2() throws EmptyCollectionException{
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("hola");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1, lista2);
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestmayorLongitud/testEditorUnico.txt");
         assertEquals("hola", editor.mayorLongitud());
     }
 
     @DisplayName("Test para camino 1-2-3-4-5-6-7-8-10-6-4-16")
     @Test
     public void PruebamayorLongitud3() throws EmptyCollectionException{
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("hola", "uno");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("ya");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1, lista2);
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestmayorLongitud/testEditorCorrecto1.txt");
         assertEquals("hola", editor.mayorLongitud());
     }
 
     @DisplayName("Test para camino 1-2-3-4-5-6-7-8-10-11-6-4-16")
     @Test
     public void PruebamayorLongitud4() throws EmptyCollectionException{
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("hola", "uno");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("coche");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1, lista2);
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestmayorLongitud/testEditorCorrecto2.txt");
         assertEquals("coche", editor.mayorLongitud());
     }
 }
--- src/test/java/com/practica/cajablanca/pruebasnumPalabras.java
+++ src/test/java/com/practica/cajablanca/pruebasnumPalabras.java
@@ -1,7 +1,6 @@
 package com.practica.cajablanca;
 
 import com.cajanegra.EmptyCollectionException;
-import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,67 +11,49 @@ public class pruebasnumPalabras {
     @DisplayName("Test para camino 1-2-3-1")
     @Test
     public void PruebanumPalabras() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("adios", "hola");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("uno", "dos");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1,lista2);
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorCorrecto.txt");
         assertThrows(IllegalArgumentException.class, () -> editor.numPalabras(-1, editor.size(), "adios"));
     }
 
     @DisplayName("Test para camino 1-2-4-5-1")
     @Test
     public void PruebanumPalabras1() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("adios", "hola");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("uno", "dos");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1,lista2);
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorCorrecto.txt");
         assertThrows(IllegalArgumentException.class, () -> editor.numPalabras(1, editor.size()+1, "adios"));
     }
 
     @DisplayName("Test para camino 1-2-4-6-7-21")
     @Test
     public void PruebanumPalabras2() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>();
-        Editor editor = new Editor(e);
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorVacio.txt");
         assertEquals(0, editor.numPalabras(1, editor.size(), "adios"));
     }
 
     @DisplayName("Test para camino 1-2-4-6-7-8-21")
     @Test
     public void PruebanumPalabras3() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("adios", "hola");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("uno", "dos");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1,lista2);
-        Editor editor = new Editor(e);
-
-        //editor = new SingleLinkedListImpl<AbstractSingleLinkedListImpl<String>("")>("");
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorCorrecto.txt");
         assertEquals(0, editor.numPalabras(editor.size()+1, editor.size(), "adios"));
     }
 
     @DisplayName("Test para camino 1-2-4-6-7-8-9-10-11-18-8-21")
     @Test
     public void PruebanumPalabras4() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("uno");
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1,lista2);
-        Editor editor = new Editor(e);
-
-        //editor = new SingleLinkedListImpl<AbstractSingleLinkedListImpl<String>("")>("");
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorVacio.txt");
         assertEquals(0, editor.numPalabras(1, editor.size(), "adios"));
     }
 
     @DisplayName("Test para camino 1-2-4-6-7-8-9-10-11-18-8-21")
     @Test
     public void PruebanumPalabras5() throws EmptyCollectionException, IllegalArgumentException {
-        SingleLinkedListImpl<String> lista1 = new SingleLinkedListImpl<String>("adios", "adios");
-        SingleLinkedListImpl<String> lista2 = new SingleLinkedListImpl<String>("uno", "adios" );
-        SingleLinkedListImpl<SingleLinkedListImpl<String>> e = new SingleLinkedListImpl<SingleLinkedListImpl<String>>(lista1,lista2);
-        Editor editor = new Editor(e);
-
-        //editor = new SingleLinkedListImpl<AbstractSingleLinkedListImpl<String>("")>("");
-        assertEquals(3, editor.numPalabras(1, editor.size(), "adios"));
+        Editor editor = new Editor();
+        editor.leerFichero("src/test/java/com/practica/cajablanca/TestnumPalabras/testEditorCorrecto.txt");
+        assertEquals(2, editor.numPalabras(1, editor.size(), "adios"));
     }
 
 
